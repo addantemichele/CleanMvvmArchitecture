dd384e3e46ee98d2abf11c78ea14fd49570adfbb
Michele Addante <addante.mic@gmail.com>
2023-01-03 11:05:09 +0100

    Initial Commit

---
+++ CleanMvvmArchitecture/CleanMvvmArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanMvvmArchitectureApp.swift
+//  CleanMvvmArchitecture
+//
+//  Created by michele addante on 03/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CleanMvvmArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
